13310bfd7651a3fae43fdd71a91a6a7b438ccd91
Sandro Gakharia <sandrogakharia@gmail.com>
2026-03-30 20:52:08 +0400

    refactor: clean project structure and remove expo router

---
+++ src/theme/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  background: '#F3F3F3',
+  white: '#FFFFFF',
+  text: '#111111',
+  muted: '#8A8A8A',
+  primary: '#3797F0',
+  primaryLight: '#D8EBFF',
+  border: '#D9D9D9',
+  darkBorder: '#4A4A4A',
+  icon: '#6B6B6B',
+  red: '#F45B5B',
+};
